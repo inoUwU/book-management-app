d896888c9aa4edd4fe44c4bc398d163b40bdf471
ino <87593133+inoUwU@users.noreply.github.com>
2025-02-04 23:46:58 +0900

    書籍管理機能を追加し、書籍リストを取得するAPIエンドポイントを実装

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,45 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+type BookManager = {
+  id: number,
+  name: string,
+  status: string // TODO to const enum
+}
+
+const books: BookManager[] = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    status: "Available"
+  },
+  {
+    id: 2,
+    name: "The Lord of the Rings",
+    status: "Not Available"
+  },
+  {
+    id: 3,
+    name: "The Hobbit",
+    status: "loaned"
+  }
+]
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-const port = 3000
+app.get("/books", (c) => {
+
+  const query = c.req.query();
+  const keyword = query.keyword;
+  if (keyword) {
+    return c.json(books.filter(book => book.name.includes(keyword)));
+  }
+  return c.json(books);
+})
+
+const port = 8000
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
