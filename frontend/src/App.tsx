03a3f5ea36ab1bf17b77fd5b4d00172bf0b0b5c7
ino <87593133+inoUwU@users.noreply.github.com>
2025-02-08 00:11:23 +0900

    書籍管理機能を実装し、書籍データを取得して表示するコンポーネントを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,30 @@
+import { use } from "react"
+import { BookManage } from "./domain/book";
+
+
+async function fetchBookMange(): Promise<BookManage[]> {
+  // サスペンドを体感するために1秒待つ
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  const response = await fetch('http://localhost:8000/books')
+  const data = await response.json();
+  return data.map((book: { id: number; name: string; status: string; }) => new BookManage(book.id, book.name, book.status));
+}
+
+const fetchBookMangePromise = fetchBookMange();
 
 function App() {
+  // use でサスペンド可能な関数を呼び出す
+  const initialBooks = use(fetchBookMangePromise);
 
   return (
     <>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+      <div>
+        <ul>
+          {initialBooks.map(book => (
+            <li key={book.id}>{book.name} - {book.status}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
